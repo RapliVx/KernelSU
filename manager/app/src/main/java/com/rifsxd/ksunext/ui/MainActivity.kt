9b7c1e4ffb889e7011f5d2ba2089664a973a3903
Suraj J Pai <11532648+fatalcoder524@users.noreply.github.com>
2025-12-15 23:54:48 +0530

    Merge pull request #936 from fatalcoder524/add-shortcuts
    Add Support for App Shorcuts

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/MainActivity.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/MainActivity.kt
@@ -5,13 +5,16 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -23,6 +26,9 @@ import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.NavHostAnimatedDestinationStyle
 import com.ramcosta.composedestinations.generated.NavGraphs
 import com.ramcosta.composedestinations.generated.destinations.FlashScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.ModuleScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.SuperUserScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.SettingScreenDestination
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import com.rifsxd.ksunext.Natives
 import com.rifsxd.ksunext.ui.screen.FlashIt
@@ -30,6 +36,10 @@ import com.rifsxd.ksunext.ui.theme.KernelSUTheme
 import com.rifsxd.ksunext.ui.util.*
 
 class MainActivity : ComponentActivity() {
+
+    var zipUri: ArrayList<Uri>? = null
+    var navigateLoc by mutableStateOf("")
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase?.let { LocaleHelper.applyLanguage(it) })
@@ -46,16 +56,13 @@ class MainActivity : ComponentActivity() {
         val isManager = Natives.isManager
         if (isManager) install()
 
-        val zipUri: ArrayList<Uri>? = if (intent.data != null) {
-            arrayListOf(intent.data!!)
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                intent.getParcelableArrayListExtra("uris", Uri::class.java)
-            } else {
-                @Suppress("DEPRECATION")
-                intent.getParcelableArrayListExtra("uris")
-            }
+        if ((intent.flags and Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY) != 0) {
+            intent.extras?.clear()
+            intent = null
         }
+
+        if(intent != null)
+            handleIntent(intent)
 
         setContent {
             val prefs = getSharedPreferences("settings", MODE_PRIVATE)
@@ -66,14 +73,31 @@ class MainActivity : ComponentActivity() {
                 val snackBarHostState = remember { SnackbarHostState() }
                 val navigator = navController.rememberDestinationsNavigator()
 
-                LaunchedEffect(zipUri) {
+                LaunchedEffect(zipUri, navigateLoc) {
                     if (!zipUri.isNullOrEmpty()) {
                         navigator.navigate(
                             FlashScreenDestination(
-                                flashIt = FlashIt.FlashModules(zipUri),
+                                flashIt = FlashIt.FlashModules(zipUri!!),
                                 finishIntent = true
                             )
                         )
+                        zipUri = null
+                    }
+
+                    if(zipUri.isNullOrEmpty() && navigateLoc != "")
+                    {
+                        when(navigateLoc) {
+                            "superuser" -> {
+                                navigator.navigate(SuperUserScreenDestination)
+                            }
+                            "modules" -> {
+                                navigator.navigate(ModuleScreenDestination)
+                            }
+                            "settings" -> {
+                                navigator.navigate(SettingScreenDestination)
+                            }
+                        }
+                        navigateLoc = ""
                     }
                 }
 
@@ -84,7 +108,9 @@ class MainActivity : ComponentActivity() {
                         LocalSnackbarHost provides snackBarHostState,
                     ) {
                         DestinationsNavHost(
-                            modifier = Modifier.padding(innerPadding).windowInsetsPadding(WindowInsets.navigationBars),
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .windowInsetsPadding(WindowInsets.navigationBars),
                             navGraph = NavGraphs.root,
                             navController = navController,
                             defaultTransitions = object : NavHostAnimatedDestinationStyle() {
@@ -116,5 +142,38 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        // Handle cases where the app is already running when a shortcut is tapped
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        when (intent.action) {
+            "com.rifsxd.ksunext.ACTION_SETTINGS" -> {
+                navigateLoc = "settings"
+            }
+            "com.rifsxd.ksunext.ACTION_SUPERUSER" -> {
+                navigateLoc = "superuser"
+            }
+            "com.rifsxd.ksunext.ACTION_MODULES" -> {
+                navigateLoc = "modules"
+            }
+            "android.intent.action.VIEW" -> {
+                zipUri = if (intent.data != null) {
+                    arrayListOf(intent.data!!)
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableArrayListExtra("uris", Uri::class.java)
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intent.getParcelableArrayListExtra("uris")
+                    }
+                }
+            }
+        }
+        setIntent(null)
     }
 }
