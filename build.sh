a5d5fb953c58c51ce073b728c4253b965198ca75
KOWX712 <leecc0503@gmail.com>
2025-12-15 21:11:07 +0800

    personal

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p out
+
+rustup default stable
+
+. out/sign.properties
+
+export ORG_GRADLE_PROJECT_KEYSTORE_FILE="$KEYSTORE_FILE"
+export ORG_GRADLE_PROJECT_KEYSTORE_PASSWORD="$KEYSTORE_PASSWORD"
+export ORG_GRADLE_PROJECT_KEY_ALIAS="$KEY_ALIAS"
+export ORG_GRADLE_PROJECT_KEY_PASSWORD="$KEY_PASSWORD"
+
+export ANDROID_NDK_HOME=/opt/android-sdk/ndk/29.0.14206865
+export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$HOME/.cargo/bin:$PATH"
+export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang"
+
+just bm
+
+rm -f out/*.apk
+cp -f manager/app/build/outputs/apk/release/*.apk out/
